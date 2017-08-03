c6e0208439386bb2655ac9774fa08d11b1034eb5
Mateusz Przybyla <inz.mateusz.przybyla@gmail.com>
2017-08-03 17:57:35 +0200

    Working on ukf

---
+++ examples/ukf_example.cpp
@@ -0,0 +1,187 @@
+/*
+ * In this example we show how to estimate the angle of a pendulum attached to
+ * a frame moving in vertical direction. The length of the pendulum is "d", the
+ * mass (concentrated at pendulum end point) of the frame is "m", the gravity
+ * acceleration is "g" and there is a friction coefficient "b" at the bearing.
+ *
+ * The state of the system is q = (phi, omega) (angle and angular velocity):
+ * n = 2
+ * The input of the system is u = (a) (linear acceleration pointing up): l = 1
+ * The output of the system is y = (a, b) (position of the
+ */
+
+#include <iostream>
+#include <vector>
+#include <random>
+
+#include "../unscented_kalman_filter.h"
+#include "matplotlibcpp.h"
+
+namespace plt = matplotlibcpp;
+
+using namespace arma;
+using namespace std;
+using namespace kf;
+
+// Time and samples
+const double system_dt = 0.01;
+const double measurement_dt = 0.05;
+const double simulation_time = 10.0;
+
+int N = static_cast<int>(simulation_time / system_dt);
+int M = static_cast<int>(measurement_dt / system_dt);
+
+// Process constants
+const double m = 1.0;   // Mass in kg
+const double g = 9.8;   // Gravitational accel.
+const double d = 1.0;   // Length in m
+const double b = 0.5;   // Friction coef. in 1/s
+
+// Process function as standard function
+vec processFunction(vec q, vec u) {
+  vec q_pred = vec(2).zeros();
+
+  q_pred(0) = q(0) + q(1) * system_dt;
+  q_pred(1) = q(1) + (m * (g + u(0)) * d * sin(q(0)) - b * q(1)) * system_dt /
+              (m * d * d);
+
+  return q_pred;
+}
+
+// Output function as lambda
+auto outputFunction = [](vec q)->vec{
+  return {d * sin(q(0)), d * cos(q(0))}; };
+
+// Process Jacobian as member function
+struct ProcessJacobian {
+  mat processJacobian(vec q, vec u) {
+    double a11 = 1.0;
+    double a12 = system_dt;
+    double a21 = (g + u(0)) * cos(q(0)) * system_dt / d;
+    double a22 = 1.0 - b * system_dt / (m * d * d);
+
+    return { {a11, a12},
+             {a21, a22} };
+  }
+};
+
+// Output Jacobian as function object
+struct outputJacobian {
+  mat operator()(vec q) const {
+    return { { d * cos(q(0)), 0.0},
+             {-d * sin(q(0)), 0.0} };
+  }
+};
+
+
+int main() {
+//  // Buffers for plots
+//  vector<double> time(N);
+
+//  vector<double> true_ang(N);
+//  vector<double> true_vel(N);
+//  vector<double> true_acc(N);
+
+//  vector<vec> measured_xy(N);
+//  vector<double> measured_ang(N);
+//  vector<double> estimated_ang(N);
+//  vector<double> believed_acc(N);
+
+//  // Pseudo random numbers generator
+//  double measurement_mu = 0.0;      // Mean
+//  double measurement_sigma = 0.1;   // Standard deviation
+
+//  double process_mu = 0.0;
+//  double process_sigma = 0.05;
+
+//  default_random_engine generator;
+//  normal_distribution<double> measurement_noise(measurement_mu, measurement_sigma);
+//  normal_distribution<double> process_noise(process_mu, process_sigma);
+
+//  // Preparing KF
+//  ExtendedKalmanFilter ekf(1, 2, 2);
+
+//  // Use standard function
+//  ekf.setProcessFunction(processFunction);
+
+//  // Use predefined lambda
+//  ekf.setOutputFunction(outputFunction);
+
+//  // Use member function
+//  ProcessJacobian s;
+//  using std::placeholders::_1;
+//  using std::placeholders::_2;
+//  ekf.setProcessJacobian(std::bind(&ProcessJacobian::processJacobian, &s, _1, _2));
+
+//  // Use function object
+//  ekf.setOutputJacobian(outputJacobian());
+
+//  mat Q = {{0.001, 0.0}, {0.0, 0.001}};
+//  mat R = {{1.0, 0.0}, {0.0, 1.0}};
+
+//  ekf.setProcessCovariance(Q);
+//  ekf.setOutputCovariance(R);
+
+//  // Initial values (unknown by EKF)
+//  time[0] = 0.0;
+
+//  true_ang[0] = 1.0;
+//  true_vel[0] = 0.0;
+//  true_acc[0] = 0.0;
+
+//  measured_xy[0] = {0.0, 0.0};
+//  measured_ang[0] = 0.0;
+//  estimated_ang[0] = 0.0;
+//  believed_acc[0] = 0.0;
+
+//  // Simulation
+//  for (int i = 1; i < N; ++i) {
+//    time[i] = i * system_dt;
+
+//    // We belive that acceleration was this
+//    believed_acc[i] = sin(time[i] * 2.0 * M_PI / simulation_time);
+
+//    // In fact there was some noise on input
+//    true_acc[i] = believed_acc[i] + process_noise(generator);
+
+//    // We use the process function to simulate the system
+//    vec q = processFunction({true_ang[i - 1], true_vel[i - 1]}, {true_acc[i - 1]});
+//    true_vel[i] = q(1);
+//    true_ang[i] = q(0);
+
+//    // New measurement comes once every M samples of the system
+//    if (i % M == 0) {
+//      measured_xy[i] = outputFunction({true_ang[i]});
+//      measured_xy[i](0) += measurement_noise(generator);
+//      measured_xy[i](1) += measurement_noise(generator);
+//      measured_ang[i] = atan2(measured_xy[i](0), measured_xy[i](1));
+//      if (measured_ang[i] < 0.0)
+//        measured_ang[i] += 2.0 * M_PI;
+//    }
+//    else {
+//      measured_xy[i] = measured_xy[i - 1];
+//      measured_ang[i] = measured_ang[i - 1];
+//    }
+
+//    // Here we do the magic
+//    ekf.updateState({believed_acc[i]}, measured_xy[i]);
+
+//    estimated_ang[i] = ekf.getEstimate()(0);
+//  }
+
+//  // Plot
+//  plt::title("Estimate of angle");
+//  plt::xlabel("Time [s]");
+//  plt::ylabel("Angle [rad]");
+//  plt::named_plot("Truth", time, true_ang, "--");
+//  plt::named_plot("Measure", time, measured_ang, "-");
+//  plt::named_plot("Estimate", time, estimated_ang, "-");
+//  plt::legend();
+//  plt::grid(true);
+//  plt::fill_between(time, vector<double>(N, 0), true_ang, {{string("visible"), string("true")}, {string("alpha"), string("0.4")}});
+//  plt::xlim(0.0, simulation_time - system_dt);
+//  plt::save("./ekf_result.png");
+//  plt::show();
+
+  return 0;
+}
--- unscented_kalman_filter.h
+++ unscented_kalman_filter.h
@@ -35,7 +35,11 @@ public:
    * \param dim_state is the dimension of state vector
   */
   UnscentedKalmanFilter(uint dim_in, uint dim_out, uint dim_state) :
-    ExtendedKalmanFilter(dim_in, dim_out, dim_state)
+    ExtendedKalmanFilter(dim_in, dim_out, dim_state),
+    k_(2 * dim_state + 1),
+    sigma_points_(k_, arma::vec(dim_state).zeros()),
+    mean_weights_(k_, 0.0),
+    covariance_weights_(k_, 0.0)
   {}
 
   /** \brief Constructor with given state-space matrices
@@ -49,7 +53,11 @@ public:
    * \param C is the output matrix
   */
   UnscentedKalmanFilter(const arma::mat& A, const arma::mat& B, const arma::mat& C) :
-    ExtendedKalmanFilter(A, B, C)
+    ExtendedKalmanFilter(A, B, C),
+    k_(2 * n_ + 1),
+    sigma_points_(k_, arma::vec(n_).zeros()),
+    mean_weights_(k_, 0.0),
+    covariance_weights_(k_, 0.0)
   {}
 
   /** \brief Constructor with given state-space vectors
@@ -64,7 +72,11 @@ public:
    * \param q is the state vector
   */
   UnscentedKalmanFilter(const arma::vec& u, const arma::vec& y, const arma::vec& q) :
-    ExtendedKalmanFilter(u, y, q)
+    ExtendedKalmanFilter(u, y, q),
+    k_(2 * n_ + 1),
+    sigma_points_(k_, arma::vec(n_).zeros()),
+    mean_weights_(k_, 0.0),
+    covariance_weights_(k_, 0.0)
   {}
 
 
@@ -75,9 +87,40 @@ public:
     * \sa processFunction()
    */
   virtual void predictState() {
-    q_pred_ = processFunction_(q_est_, u_);
-    A_ = processJacobian_(q_est_, u_);
-    P_ = A_ * P_ * trans(A_) + Q_;
+    double alpha = 0.75;
+    double beta = 0.2;
+    double kappa = 3.0;
+    double lambda = pow(alpha, 2.0) * (n_ + kappa) - n_;
+
+    arma::mat sqrt_P = arma::chol((n_ + lambda) * P_);
+
+    // 1. Calculate sigma points
+    sigma_points_[0] = q_est_;
+    mean_weights_[0] = lambda / (n_ + lambda);
+    covariance_weights_[0] = lambda / (n_ + lambda) + (1 - pow(alpha, 2.0) + beta);
+
+    for (int i = 1; i < n_ + 1; ++i) {
+      sigma_points_[i] = q_est_ + sqrt_P.col(i - 1);
+      sigma_points_[i + n_] = q_est_ - sqrt_P.col(i - 1);
+
+      mean_weights_[i] = covariance_weights_[i] = 1.0 / (2.0 * (n_ + lambda));
+      mean_weights_[i + n_] = covariance_weights_[i + n_] = 1.0 / (2.0 * (n_ + lambda));
+    }
+
+    // 2. Predict new points based on sigma points
+    for (int i = 0; i < k_; ++i)
+      pred_sigma_points_[i] = processFunction_(sigma_points_[i], u_);
+
+    // 3. Calculate mean of predicted points
+    q_pred_ = arma::vec(n_).zeros();
+    for (int i = 0; i < k_; ++i) {
+      q_pred_ += mean_weights_[i] * pred_sigma_points_[i];
+    }
+
+    // 4. Caclulate covariance of predicted points
+    P_ = Q_;
+    for (int i = 0; i < k_; ++i)
+      P_ += covariance_weights_[i] * (pred_sigma_points_[i] - q_pred_) * trans(pred_sigma_points_[i] - q_pred_);
   }
 
   /** \brief Performs the UKF prediction step given the input vector
@@ -99,11 +142,26 @@ public:
    * \sa outputFunction_()
   */
   virtual void correctState() {
-    C_ = outputJacobian_(q_est_);
-    S_ = C_ * P_ * trans(C_) + R_;
-    K_ = P_ * trans(C_) * inv(S_);
-    q_est_ = q_pred_ + K_ * (y_ - outputFunction_(q_pred_));
-    P_ = (I_ - K_ * C_) * P_;
+    // 1. Calculate output points based on predicted sigma pts
+    for (int i = 0; i < k_; ++i)
+      output_sigma_points_[i] = outputFunction_(pred_sigma_points_[i]);
+
+    arma::vec y_pred = arma::vec(m_).zeros();
+    for (int i = 0; i < k_; ++i)
+      y_pred += mean_weights_[i] * output_sigma_points_[i];
+
+    S_ = R_;
+    for (int i = 0; i < k_; ++i)
+      S_ += covariance_weights_[i] * (output_sigma_points_[i] - y_pred) * trans(output_sigma_points_[i] - y_pred);
+
+    arma::mat Sth = arma::mat(n_, m_).zeros();
+    for (int i = 0; i < k_; ++i)
+      Sth += covariance_weights_[i] * (pred_sigma_points_[i] - q_pred_) * trans(output_sigma_points_[i] - y_pred);
+
+    K_ = Sth * inv(S_);
+
+    q_est_ = q_pred_ + K_ * (y_ - y_pred);
+    P_ = P_ - K_ * S_ * trans(K_);
   }
 
   /** \brief Performs the UKF correction step given the output vector
@@ -131,6 +189,15 @@ private:
    * in this context.
   */
   void setOutputJacobian() {}
+
+  uint k_;  /**< \brief Number of sigma points */
+
+  std::vector<arma::vec> sigma_points_;         /**< \brief Points for distribution propagation */
+  std::vector<arma::vec> pred_sigma_points_;    /**< \brief Points for distribution propagation */
+  std::vector<arma::vec> output_sigma_points_;  /**< \brief Points for distribution propagation */
+
+  std::vector<double> mean_weights_;        /**< \brief Weights for mean propagation */
+  std::vector<double> covariance_weights_;  /**< \brief Weights for covariance propagation */
 };
 
 } // namespace kf
